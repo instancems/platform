3965e2f81cd89c3afa3c7bd62fda23cbb9704766
instancems <mono.instancems@gmail.com>
2016-11-22 10:18:43 +0700

    fix composer and folder structure

---
+++ src/Foundation/Application.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace InstanCeMS\Foundation;
+
+use Illuminate\Foundation\Application as LaravelApps;
+
+class Application extends LaravelApps
+{
+	/**
+     * The InstanCeMS Platform version.
+     *
+     * @var string
+     */
+    const VERSION = '0.0.10';
+}
---
+++ src/Foundation/Http/Kernel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace InstanCeMS\Foundation\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+	// 
+}
